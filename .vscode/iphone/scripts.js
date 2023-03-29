d06ba86ca4900f0f65210757d760d757c039ade7
felipesilva0425 <felipesilva061991@gmail.com>
2023-03-29 00:27:17 -0300

    Primeiro Commit - Projeto Iphone

---
+++ .vscode/iphone/scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagem(imagem){
+   let imgiphone = document.querySelector(".iphone")
+   imgiphone.src = imagem
+}
