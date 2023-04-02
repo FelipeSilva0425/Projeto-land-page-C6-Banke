f421f5c3d1997ec9d979b0a519c83bb7f3404ab0
felipesilva0425 <felipesilva061991@gmail.com>
2023-04-02 15:53:41 -0300

    Starbucks projeto

--- .vscode/iphone/scripts.js
+++
@@ -1,9 +0,0 @@
-function trocaCor(cor){
-    let circulo = document.querySelector(".circulo")
-    circulo.style.background = cor
-}
-
-function trocaImagem(imagem){
-   let imgiphone = document.querySelector(".iphone")
-   imgiphone.src = imagem
-}
